d664f4077303fed445b133f615cc1253088a297d
AdrianGjerstad <48812634+AdrianGjerstad@users.noreply.github.com>
2021-04-20 09:40:14 -0400

    [chore] Run cpplint

--- cookiec/test/fs.cc
+++ cookiec/test/fs.cc
@@ -1,38 +1,63 @@
+// Copyright 2021 Adrian Gjerstad
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <unistd.h>
+
 #include <iostream>
 #include <string>
 #include <vector>
 #include <deque>
-#include <unistd.h>
 
 #include "../src/include/util/Filesystem.h"
-
 #include <igloo/igloo_alt.h>
-using namespace igloo;
 
 Describe(filesystem) {
   Describe(paths) {
     It(normalizes_paths) {
-      Assert::That(cookie::fs::normalize_path("/foo/bar", "../baz"), Equals("/foo/baz"));
+      igloo::Assert::That(
+          cookie::fs::normalize_path("/foo/bar", "../baz"),
+          igloo::Equals("/foo/baz"));
     }
 
     It(forces_absolute_paths) {
-      Assert::That(cookie::fs::absolute_path("foo"), Equals("/home/foo"));
+      igloo::Assert::That(
+          cookie::fs::absolute_path("foo"),
+          igloo::Equals("/home/foo"));
     }
 
     It(forces_relative_paths) {
-      Assert::That(cookie::fs::relative_path("/hello"), Equals("../hello"));
+      igloo::Assert::That(
+          cookie::fs::relative_path("/hello"),
+          igloo::Equals("../hello"));
     }
 
     It(produces_path_segments) {
-      Assert::That(cookie::fs::path_segments("/foo/bar/baz"), Equals<std::deque<std::string>>({"foo", "bar", "baz"}));
+      igloo::Assert::That(
+          cookie::fs::path_segments("/foo/bar/baz"),
+          igloo::Equals<std::deque<std::string>>({"foo", "bar", "baz"}));
     }
 
     It(produces_pwd) {
-      Assert::That(cookie::fs::pwd(), Equals("/home"));
+      igloo::Assert::That(
+          cookie::fs::pwd(),
+          igloo::Equals("/home"));
     }
 
     It(purges_double_slashes) {
-      Assert::That(cookie::fs::remove_doubleslash("/foo//bar//baz/ham"), Equals("/foo/bar/baz/ham"));
+      igloo::Assert::That(
+          cookie::fs::remove_doubleslash("/foo//bar//baz/ham"),
+          igloo::Equals("/foo/bar/baz/ham"));
     }
   };
 };
@@ -40,6 +65,6 @@ Describe(filesystem) {
 int main(int argc, char** argv) {
   chdir("/home");
 
-  return TestRunner::RunAllTests(argc, argv);
+  return igloo::TestRunner::RunAllTests(argc, argv);
 }
 
--- cookiec/test/source-code-pool.cc
+++ cookiec/test/source-code-pool.cc
@@ -1,26 +1,36 @@
+// Copyright 2021 Adrian Gjerstad
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include <iostream>
 #include <string>
 #include <vector>
 #include <fstream>
 
-#include <igloo/igloo_alt.h>
-using namespace igloo;
-
 #include "../src/include/util/SourceCodePool.h"
+#include <igloo/igloo_alt.h>
 
 Describe(source_code_pool) {
-  void SetUp() {
-    
-  }
+  void SetUp() {}
 
   It(reads_files_correctly) {
     cookie::SourceCodePool pool;
-    
+
     const std::string filename = "cookiec/test/data/code.txt";
     std::ifstream f(filename);
     f.seekg(0, f.end);
     unsigned int size = f.tellg();
-    char c[size+1];
+    char c[size + 1];
     f.seekg(0, f.beg);
     f.read(c, size);
     f.close();
@@ -28,31 +38,32 @@ Describe(source_code_pool) {
     const std::string content = c;
 
     pool.add(filename);
-    
+
     const std::string* source = pool.get(filename);
     // DO NOT DEREFERENCE THE STRING IN REAL CODE WITH UNKNOWN SIZES OF DATA!!!!
-    Assert::That(*source, Equals(content));
+    igloo::Assert::That(*source, igloo::Equals(content));
   }
 
   It(deletes_files_correctly) {
     cookie::SourceCodePool pool;
-    
-    pool.add("/etc/passwd");  // Just an example file that is guaranteed to exist.
+
+    // Just an example file that is guaranteed to exist.
+    pool.add("/etc/passwd");
     pool.remove("/etc/passwd");
 
-    Assert::That(pool.get("/etc/passwd"), Equals(nullptr));
+    igloo::Assert::That(pool.get("/etc/passwd"), igloo::Equals(nullptr));
   }
 
   It(disposes_files_correctly) {
     cookie::SourceCodePool pool;
-    
+
     pool.add("/etc/passwd");
     pool.add("/etc/group");
-    
+
     // Emulate destruction of the pool. Destroy/free all source code resources.
     pool.dispose();
 
-    Assert::That(pool.size(), Equals(0));
+    igloo::Assert::That(pool.size(), igloo::Equals(0));
   }
 
   It(handles_missing_files_correctly) {
@@ -60,11 +71,11 @@ Describe(source_code_pool) {
 
     pool.add("/nonexistent");
 
-    Assert::That(pool.get("/nonexistent"), Equals(nullptr));
+    igloo::Assert::That(pool.get("/nonexistent"), igloo::Equals(nullptr));
   }
 };
 
 int main(int argc, char** argv) {
-  return TestRunner::RunAllTests(argc, argv);
+  return igloo::TestRunner::RunAllTests(argc, argv);
 }
 
